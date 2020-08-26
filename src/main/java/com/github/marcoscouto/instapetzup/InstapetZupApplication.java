56167939e097ef4f2cad86cdbacd758a63f1d558
Marcos Couto <marcos_otuoc@hotmail.com>
2020-08-26 09:27:02 -0300

    Initial commit

---
+++ src/main/java/com/github/marcoscouto/instapetzup/InstapetZupApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcoscouto.instapetzup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstapetZupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstapetZupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/marcoscouto/instapetzup/InstapetZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcoscouto.instapetzup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstapetZupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
