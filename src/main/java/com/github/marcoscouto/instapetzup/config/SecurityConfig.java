e9395d26b8748dca383ea2ac29694b975125eacf
Marcos Couto <marcos_otuoc@hotmail.com>
2020-08-28 21:28:47 -0300

    Swagger Security

--- src/main/java/com/github/marcoscouto/instapetzup/config/SecurityConfig.java
+++ src/main/java/com/github/marcoscouto/instapetzup/config/SecurityConfig.java
@@ -56,7 +56,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/h2-console/**");
+        web.ignoring().antMatchers(
+                "/h2-console/**",
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 
     @Bean
--- src/main/java/com/github/marcoscouto/instapetzup/controllers/AdminController.java
+++ src/main/java/com/github/marcoscouto/instapetzup/controllers/AdminController.java
@@ -2,6 +2,7 @@ package com.github.marcoscouto.instapetzup.controllers;
 
 import com.github.marcoscouto.instapetzup.dto.PermissionDTO;
 import com.github.marcoscouto.instapetzup.services.impl.PetService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "Admin")
 @RequestMapping("/admin")
 public class AdminController {
 
