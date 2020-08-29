51ceb5c76bc9c8c55a55b3740e3e9cbf136def0a
Marcos Couto <marcos_otuoc@hotmail.com>
2020-08-29 16:11:23 -0300

    utf-8

---
+++ src/main/java/com/github/marcoscouto/instapetzup/config/EncodeConfig.java
@@ -0,0 +1,17 @@
+package com.github.marcoscouto.instapetzup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class EncodeConfig {
+
+    @Bean
+    public CharacterEncodingFilter getCharacterEncodingFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
+}
